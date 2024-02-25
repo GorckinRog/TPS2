528b76be61be0f3b76f2ab5e7ea7b5c75ca033b8
GorckinRog <nick.gorodetskiy@gmail.com>
2024-02-25 22:04:30 +0300

    a

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
+
+public class Enemy : MonoBehaviour
+{
+    public List<Transform> PatrolPoints;
+    public Transform player;
+    public float ViewAngle = 45;
+
+    private NavMeshAgent _navMeshAgent;
+    private bool isPLayerNoticed;
+
+    private void Start()
+    {
+        InitComponentLinks();
+
+        PickNewPatrolPoint();
+    }
+
+    private void InitComponentLinks()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        var direction = player.transform.position - transform.position;
+    }
+}
+
+
